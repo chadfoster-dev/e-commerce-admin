7080ea6a89ea008320d56e81754d3dd9e4828315
Chad Foster <fosterchad95@gmail.com>
2023-08-31 00:32:04 -0400

    add global modals

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 const SetupPage = () => {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div>Root Page</div>;
 };
 
 export default SetupPage;
